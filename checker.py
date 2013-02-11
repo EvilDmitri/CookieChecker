4abb902f7806e4eb3ef313e44c95256fbe52fc86
EvilDmitri <dmitribrach@gmail.com>
2013-02-11 19:58:00 +0200

    to test

---
+++ checker.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from grab.spider import Spider, Task
+from grab import Grab
+
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+threads = 1
+urls_file = 'urls.txt'
+found_file = 'found.txt'
+not_found_file = 'not_found.txt'
+
+class CookieSpider(Spider):
+
+    found = []
+    not_found = []
+    errors = []
+
+    def task_generator(self):
+        self.errors = prepare_errors()
+
+        with open(urls_file) as f:
+            for url in f:
+                if url:
+                    grab = Grab()
+                    grab.setup(url=str(url).rstrip('\n'))
+                    print "Test in progress for the - ", url
+                    yield Task('initial', url=str(url), grab=grab, )
+
+
+    def task_initial(self, grab, task):
+        cookies = grab.response.cookies
+
+        for cookie in cookies.iterkeys():
+            new_cookies = cookies
+            new_cookies[cookie] += "'"
+            grab = Grab()
+            grab.setup(cookies=new_cookies)
+            grab.setup(url=task.url)
+            self.add_task(Task('check', url=task.url, grab=grab, old_cookies=cookies, new_cookies=new_cookies))
+
+
+    def task_check(self, grab, task):
+        raw_text = grab.xpath_text('//*')
+        for error in self.errors:
+            if re.findall(error, raw_text):
+                self.write_file(found_file, task.url+'-'+task.new_cookies)
+            else:
+                if not task.url in self.not_found:
+                    self.not_found.append(task.url)
+                pass
+        if self.not_found:
+            self.write_file(not_found_file, task.url)
+
+    def write_file(self, file, url):
+        with open(file, 'a') as f:
+            f.write(url+'\n')
+
+
+def prepare_errors():
+    errors = []
+    with open('errors.txt') as f:
+        for error in f:
+            errors.append(re.compile(error))
+    return errors
+
+
+def main():
+
+    bot = CookieSpider(thread_number=threads,network_try_limit=10)
+
+    try: bot.run()
+    except KeyboardInterrupt: pass
+
+    print 'All done'
+    #print bot.render_stats()
+
+if __name__ == '__main__':
+    print 'Start working'
+    main()
+
+
+
